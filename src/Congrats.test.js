e16393ef31da55b04c952f16d53218e3fc5625d3
Joe Contreras <joe.contreras809@gmail.com>
2022-02-17 20:36:15 -0600

    Start tests for GuessedWords

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,9 +1,6 @@
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { shallow } from "enzyme";
 import Congrats from "./Congrats";
 import { findByTestAttr } from "../test/testUtils";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const setup = (props = {}) => {
   return shallow(<Congrats {...props} />);
@@ -14,11 +11,13 @@ test("renders without error", () => {
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
+
 test("renders no text when 'success' prop is false", () => {
   const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.text()).toBe("");
 });
+
 test("renders non-empty success message when 'success' prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
